3bc77cbe3aa2a96ba781be8647ed7b8c0f0c9ab2
nirajkumarvovradeveloper <80186270+nirajkumarvovradeveloper@users.noreply.github.com>
2021-05-02 10:01:50 -0400

    commit

---
+++ src/components/auth/Register3.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export const Register3 = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Register3.propTypes = {
+    props: PropTypes
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register3)
