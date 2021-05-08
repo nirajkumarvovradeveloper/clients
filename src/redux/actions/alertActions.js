0cb9c98b1102567141dd7a4330c75308bd1da1ae
nirajkumarvovradeveloper <80186270+nirajkumarvovradeveloper@users.noreply.github.com>
2021-05-07 23:29:24 -0400

    commit

--- src/redux/actions/alertActions.js
+++ src/redux/actions/alertActions.js
@@ -1,3 +1,11 @@
+import { v4 as uuid} from "uuid";
+import { SET_ALERT } from "../types";
+export const setAlert = (msg, alertType, timeout = 5000)=> (dispatch) => {
+    // display the alerts
+    const id = uuid();
+    dispatch({
+        type : SET_ALERT
+    })
 
-
-export const setAlert = (msg, alertType, timeout = 5000);
+    //after specified time/default time ===> alerts should be disappeared.
+};
