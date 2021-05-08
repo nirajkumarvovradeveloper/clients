c33b7a85e4ab629950d3c9955b044eada471a455
nirajkumarvovradeveloper <80186270+nirajkumarvovradeveloper@users.noreply.github.com>
2021-05-08 07:26:32 -0400

    commit

--- src/redux/actions/alertActions.js
+++ src/redux/actions/alertActions.js
@@ -1,11 +1,16 @@
 import { v4 as uuid} from "uuid";
-import { SET_ALERT } from "../types";
+import { REMOVE_ALERT, SET_ALERT } from "../types";
 export const setAlert = (msg, alertType, timeout = 5000)=> (dispatch) => {
     // display the alerts
     const id = uuid();
     dispatch({
-        type : SET_ALERT
-    })
+        type : SET_ALERT,
+        payload: { msg, alertType, id },
+    });
+
+    setTimeout(() =>{
+        dispatch({ type: REMOVE_ALERT, payload: id }),
+    })(timeout); // 1:32 check
 
     //after specified time/default time ===> alerts should be disappeared.
 };
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,7 @@
 import {combinedReducers} from "redux";
 
+import alert from "../actions/"; // 1:34
 export default combinedReducers({
     auth, //1:30
+    alert,
 });
