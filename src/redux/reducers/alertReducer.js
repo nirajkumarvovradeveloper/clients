0a61acd2f27230a18ceac179899a83f4ed5bfb01
nirajkumarvovradeveloper <80186270+nirajkumarvovradeveloper@users.noreply.github.com>
2021-05-07 23:17:24 -0400

    commit

--- src/redux/reducers/alertReducer.js
+++ src/redux/reducers/alertReducer.js
@@ -1,14 +1,13 @@
-const initialState = {
-
-};
+import { REMOVE_ALERT, SET_ALERT } from "../types";
+const initialState = [];
 
 export default (state = initialState, action) => {
     const { type, payload } = action;
     switch (type) {
     case SET_ALERT:
-        return { ...state, ...payload };
+        return [ ...state, ...payload ];
     case REMOVE_ALERT:
-        return { ...state, ...payload };
+        return;
     case typeName:
         return;
 
